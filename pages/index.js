f3238396217406a90c475d904bc5c72a4c05a644
Chu Hoang Son <chuson1996@gmail.com>
2022-07-14 13:50:00 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(() => {
+    window.location.href = 'https://upez.framer.website';
+  }, [])
   return (
     <div className="container">
       <Head>
